2d098d2ba1244358b6aa80d1e114b90098c718ce
NigelMupira <h230828v@hit.ac.zw>
2024-09-26 17:56:47 +0200

    Update Home.cs
    Code the Sign-In, Register and Exit buttons to perform their appropriate operations

--- HIT Campus Housing Portal/Home.cs
+++ HIT Campus Housing Portal/Home.cs
@@ -19,17 +19,21 @@ namespace HIT_Campus_Housing_Portal
 
         private void btnSignIn_Click(object sender, EventArgs e)
         {
-
+            Login login = new Login();
+            login.Show(); // Show the login form
+            this.Hide();  // Hide the current form
         }
 
         private void btnSignUp_Click(object sender, EventArgs e)
         {
-
+            SignUp create = new SignUp();
+            create.Show(); // Show the sign up form
+            this.Hide(); // Hide the current form
         }
 
         private void btnExit_Click(object sender, EventArgs e)
         {
-
+            Application.Exit(); // Exit the application
         }
     }
 }
