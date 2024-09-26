942fa945efd3777dab6c6355696dbd3cdb4355f0
NigelMupira <h230828v@hit.ac.zw>
2024-09-26 17:08:01 +0200

    Design password page.

--- HIT Campus Housing Portal/Password.Designer.cs
+++ HIT Campus Housing Portal/Password.Designer.cs
@@ -28,12 +28,234 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Password));
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.groupCurrent = new System.Windows.Forms.GroupBox();
+            this.btnForgot = new System.Windows.Forms.Button();
+            this.txtOldPass = new System.Windows.Forms.TextBox();
+            this.lblOldPass = new System.Windows.Forms.Label();
+            this.groupCreate = new System.Windows.Forms.GroupBox();
+            this.lblRepeat = new System.Windows.Forms.Label();
+            this.txtRepeat = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.groupCurrent.SuspendLayout();
+            this.groupCreate.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBack.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBack.ForeColor = System.Drawing.Color.Black;
+            this.btnBack.Location = new System.Drawing.Point(286, 544);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(200, 50);
+            this.btnBack.TabIndex = 34;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = false;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(608, 87);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(128, 30);
+            this.lblUsername.TabIndex = 33;
+            this.lblUsername.Text = "USERNAME";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(402, 87);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(188, 30);
+            this.lbl1.TabIndex = 32;
+            this.lbl1.Text = "Your Username is:";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnCreate.FlatAppearance.BorderSize = 0;
+            this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreate.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreate.ForeColor = System.Drawing.Color.Black;
+            this.btnCreate.Location = new System.Drawing.Point(698, 544);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(200, 50);
+            this.btnCreate.TabIndex = 31;
+            this.btnCreate.Text = "Create Account";
+            this.btnCreate.UseVisualStyleBackColor = false;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.Black;
+            this.btnCancel.Location = new System.Drawing.Point(492, 544);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(200, 50);
+            this.btnCancel.TabIndex = 30;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // groupCurrent
+            // 
+            this.groupCurrent.Controls.Add(this.btnForgot);
+            this.groupCurrent.Controls.Add(this.txtOldPass);
+            this.groupCurrent.Controls.Add(this.lblOldPass);
+            this.groupCurrent.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupCurrent.ForeColor = System.Drawing.Color.White;
+            this.groupCurrent.Location = new System.Drawing.Point(389, 168);
+            this.groupCurrent.Name = "groupCurrent";
+            this.groupCurrent.Size = new System.Drawing.Size(406, 120);
+            this.groupCurrent.TabIndex = 29;
+            this.groupCurrent.TabStop = false;
+            this.groupCurrent.Text = "CURRENT PASSWORD";
+            // 
+            // btnForgot
+            // 
+            this.btnForgot.FlatAppearance.BorderSize = 0;
+            this.btnForgot.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForgot.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnForgot.ForeColor = System.Drawing.Color.Blue;
+            this.btnForgot.Location = new System.Drawing.Point(112, 74);
+            this.btnForgot.Name = "btnForgot";
+            this.btnForgot.Size = new System.Drawing.Size(180, 40);
+            this.btnForgot.TabIndex = 8;
+            this.btnForgot.Text = "Forgot Password?";
+            this.btnForgot.UseVisualStyleBackColor = true;
+            // 
+            // txtOldPass
+            // 
+            this.txtOldPass.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtOldPass.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOldPass.ForeColor = System.Drawing.Color.Black;
+            this.txtOldPass.Location = new System.Drawing.Point(190, 31);
+            this.txtOldPass.Name = "txtOldPass";
+            this.txtOldPass.Size = new System.Drawing.Size(200, 27);
+            this.txtOldPass.TabIndex = 1;
+            // 
+            // lblOldPass
+            // 
+            this.lblOldPass.AutoSize = true;
+            this.lblOldPass.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOldPass.ForeColor = System.Drawing.Color.Black;
+            this.lblOldPass.Location = new System.Drawing.Point(15, 35);
+            this.lblOldPass.Name = "lblOldPass";
+            this.lblOldPass.Size = new System.Drawing.Size(96, 17);
+            this.lblOldPass.TabIndex = 0;
+            this.lblOldPass.Text = "Old Password:";
+            // 
+            // groupCreate
+            // 
+            this.groupCreate.Controls.Add(this.lblRepeat);
+            this.groupCreate.Controls.Add(this.txtRepeat);
+            this.groupCreate.Controls.Add(this.txtPassword);
+            this.groupCreate.Controls.Add(this.lblPassword);
+            this.groupCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupCreate.ForeColor = System.Drawing.Color.White;
+            this.groupCreate.Location = new System.Drawing.Point(389, 322);
+            this.groupCreate.Name = "groupCreate";
+            this.groupCreate.Size = new System.Drawing.Size(406, 114);
+            this.groupCreate.TabIndex = 28;
+            this.groupCreate.TabStop = false;
+            this.groupCreate.Text = "CREATE PASSWORD";
+            // 
+            // lblRepeat
+            // 
+            this.lblRepeat.AutoSize = true;
+            this.lblRepeat.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRepeat.ForeColor = System.Drawing.Color.Black;
+            this.lblRepeat.Location = new System.Drawing.Point(15, 69);
+            this.lblRepeat.Name = "lblRepeat";
+            this.lblRepeat.Size = new System.Drawing.Size(116, 17);
+            this.lblRepeat.TabIndex = 4;
+            this.lblRepeat.Text = "Repeat Password:";
+            // 
+            // txtRepeat
+            // 
+            this.txtRepeat.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRepeat.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRepeat.ForeColor = System.Drawing.Color.Black;
+            this.txtRepeat.Location = new System.Drawing.Point(190, 65);
+            this.txtRepeat.Name = "txtRepeat";
+            this.txtRepeat.Size = new System.Drawing.Size(200, 27);
+            this.txtRepeat.TabIndex = 2;
+            this.txtRepeat.UseSystemPasswordChar = true;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.Color.Black;
+            this.txtPassword.Location = new System.Drawing.Point(190, 30);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(200, 27);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.Black;
+            this.lblPassword.Location = new System.Drawing.Point(15, 35);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(101, 17);
+            this.lblPassword.TabIndex = 0;
+            this.lblPassword.Text = "New Password:";
+            // 
+            // Password
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Goldenrod;
+            this.ClientSize = new System.Drawing.Size(1184, 681);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.groupCurrent);
+            this.Controls.Add(this.groupCreate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Password";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Password";
+            this.groupCurrent.ResumeLayout(false);
+            this.groupCurrent.PerformLayout();
+            this.groupCreate.ResumeLayout(false);
+            this.groupCreate.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.GroupBox groupCurrent;
+        private System.Windows.Forms.Button btnForgot;
+        private System.Windows.Forms.TextBox txtOldPass;
+        private System.Windows.Forms.Label lblOldPass;
+        private System.Windows.Forms.GroupBox groupCreate;
+        private System.Windows.Forms.Label lblRepeat;
+        private System.Windows.Forms.TextBox txtRepeat;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
     }
 }
