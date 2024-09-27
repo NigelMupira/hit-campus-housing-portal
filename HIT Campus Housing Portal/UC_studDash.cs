ce3677aa5bebab93a57746f7e8c6efcf29d16304
NigelMupira <h230828v@hit.ac.zw>
2024-09-27 17:47:33 +0200

    Add student user controls.
    These user control forms will serve as the actual displays of the dashboard, to be added to the display panel.

---
+++ HIT Campus Housing Portal/UC_studDash.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HIT_Campus_Housing_Portal
+{
+    public partial class UC_studDash : UserControl
+    {
+        public UC_studDash()
+        {
+            InitializeComponent();
+        }
+    }
+}
