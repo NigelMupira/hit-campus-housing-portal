9ad957057a01e9aab6a5b5b10b18cb7036da7f94
NigelMupira <h230828v@hit.ac.zw>
2024-09-27 22:23:07 +0200

    Design student dashboard user control.

--- HIT Campus Housing Portal/UC_studDash.Designer.cs
+++ HIT Campus Housing Portal/UC_studDash.Designer.cs
@@ -28,27 +28,222 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblName = new System.Windows.Forms.Label();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.groupRooms = new System.Windows.Forms.GroupBox();
+            this.panelGirls = new System.Windows.Forms.Panel();
+            this.lblGirls = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.panelBoys = new System.Windows.Forms.Panel();
+            this.lblBoys = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.panelTotal = new System.Windows.Forms.Panel();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.btnApply = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.panelTop.SuspendLayout();
+            this.groupRooms.SuspendLayout();
+            this.panelGirls.SuspendLayout();
+            this.panelBoys.SuspendLayout();
+            this.panelTotal.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblName
+            // panelTop
             // 
-            this.lblName.AutoSize = true;
-            this.lblName.Font = new System.Drawing.Font("Times New Roman", 47.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(249, 250);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(463, 72);
-            this.lblName.TabIndex = 9;
-            this.lblName.Text = "#DASHBOARD";
+            this.panelTop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelTop.Controls.Add(this.lblPath);
+            this.panelTop.Controls.Add(this.lblDisplay);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(960, 40);
+            this.panelTop.TabIndex = 26;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPath.Location = new System.Drawing.Point(815, 12);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(113, 17);
+            this.lblPath.TabIndex = 8;
+            this.lblPath.Text = "Home/Dashboard";
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(6, 12);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(75, 17);
+            this.lblDisplay.TabIndex = 7;
+            this.lblDisplay.Text = "Dashboard";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(248, 131);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(202, 25);
+            this.lbl2.TabIndex = 9;
+            this.lbl2.Text = "TOTAL ROOMS LEFT";
+            // 
+            // groupRooms
+            // 
+            this.groupRooms.Controls.Add(this.panelGirls);
+            this.groupRooms.Controls.Add(this.panelBoys);
+            this.groupRooms.Controls.Add(this.panelTotal);
+            this.groupRooms.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
+            this.groupRooms.ForeColor = System.Drawing.Color.Red;
+            this.groupRooms.Location = new System.Drawing.Point(103, 318);
+            this.groupRooms.Name = "groupRooms";
+            this.groupRooms.Size = new System.Drawing.Size(755, 310);
+            this.groupRooms.TabIndex = 27;
+            this.groupRooms.TabStop = false;
+            // 
+            // panelGirls
+            // 
+            this.panelGirls.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.panelGirls.Controls.Add(this.lblGirls);
+            this.panelGirls.Controls.Add(this.lbl4);
+            this.panelGirls.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelGirls.Location = new System.Drawing.Point(380, 196);
+            this.panelGirls.Name = "panelGirls";
+            this.panelGirls.Size = new System.Drawing.Size(346, 100);
+            this.panelGirls.TabIndex = 2;
+            // 
+            // lblGirls
+            // 
+            this.lblGirls.AutoSize = true;
+            this.lblGirls.Font = new System.Drawing.Font("Century Gothic", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGirls.Location = new System.Drawing.Point(115, 10);
+            this.lblGirls.Name = "lblGirls";
+            this.lblGirls.Size = new System.Drawing.Size(116, 56);
+            this.lblGirls.TabIndex = 13;
+            this.lblGirls.Text = "#XX";
+            // 
+            // lbl4
+            // 
+            this.lbl4.AutoSize = true;
+            this.lbl4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl4.Location = new System.Drawing.Point(86, 70);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(174, 25);
+            this.lbl4.TabIndex = 11;
+            this.lbl4.Text = "Girls\' Rooms Left";
+            // 
+            // panelBoys
+            // 
+            this.panelBoys.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panelBoys.Controls.Add(this.lblBoys);
+            this.panelBoys.Controls.Add(this.lbl3);
+            this.panelBoys.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelBoys.Location = new System.Drawing.Point(28, 196);
+            this.panelBoys.Name = "panelBoys";
+            this.panelBoys.Size = new System.Drawing.Size(346, 100);
+            this.panelBoys.TabIndex = 1;
+            // 
+            // lblBoys
+            // 
+            this.lblBoys.AutoSize = true;
+            this.lblBoys.Font = new System.Drawing.Font("Century Gothic", 35.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBoys.Location = new System.Drawing.Point(117, 10);
+            this.lblBoys.Name = "lblBoys";
+            this.lblBoys.Size = new System.Drawing.Size(113, 56);
+            this.lblBoys.TabIndex = 12;
+            this.lblBoys.Text = "#XY";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(85, 70);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(176, 25);
+            this.lbl3.TabIndex = 10;
+            this.lbl3.Text = "Boys\' Rooms Left";
+            // 
+            // panelTotal
+            // 
+            this.panelTotal.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.panelTotal.Controls.Add(this.lblTotal);
+            this.panelTotal.Controls.Add(this.lbl2);
+            this.panelTotal.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panelTotal.Location = new System.Drawing.Point(28, 20);
+            this.panelTotal.Name = "panelTotal";
+            this.panelTotal.Size = new System.Drawing.Size(698, 170);
+            this.panelTotal.TabIndex = 0;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Century Gothic", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.Location = new System.Drawing.Point(254, 24);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(191, 93);
+            this.lblTotal.TabIndex = 11;
+            this.lblTotal.Text = "#TTL";
+            // 
+            // btnApply
+            // 
+            this.btnApply.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnApply.FlatAppearance.BorderSize = 0;
+            this.btnApply.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnApply.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnApply.ForeColor = System.Drawing.Color.White;
+            this.btnApply.Location = new System.Drawing.Point(380, 231);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(200, 50);
+            this.btnApply.TabIndex = 30;
+            this.btnApply.Text = "APPLY NOW!";
+            this.btnApply.UseVisualStyleBackColor = false;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblStatus.Font = new System.Drawing.Font("Century Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(577, 155);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(157, 47);
+            this.lblStatus.TabIndex = 29;
+            this.lblStatus.Text = "#Status";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Century Gothic", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(190, 155);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(381, 47);
+            this.lbl1.TabIndex = 28;
+            this.lbl1.Text = "Application Status:";
             // 
             // UC_studDash
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.groupRooms);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lbl1);
             this.Name = "UC_studDash";
             this.Size = new System.Drawing.Size(960, 670);
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
+            this.groupRooms.ResumeLayout(false);
+            this.panelGirls.ResumeLayout(false);
+            this.panelGirls.PerformLayout();
+            this.panelBoys.ResumeLayout(false);
+            this.panelBoys.PerformLayout();
+            this.panelTotal.ResumeLayout(false);
+            this.panelTotal.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +251,21 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.GroupBox groupRooms;
+        private System.Windows.Forms.Panel panelGirls;
+        private System.Windows.Forms.Label lblGirls;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Panel panelBoys;
+        private System.Windows.Forms.Label lblBoys;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Panel panelTotal;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Button btnApply;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lbl1;
     }
 }
