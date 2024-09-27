f4e93b07d026b4627750d1e8e07277d6ff51819a
NigelMupira <h230828v@hit.ac.zw>
2024-09-27 23:07:57 +0200

    Design account settings user control.

--- HIT Campus Housing Portal/UC_studAcc.Designer.cs
+++ HIT Campus Housing Portal/UC_studAcc.Designer.cs
@@ -28,27 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblName = new System.Windows.Forms.Label();
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.lblPath = new System.Windows.Forms.Label();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.lblChange = new System.Windows.Forms.Label();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.panelHorizRule2 = new System.Windows.Forms.Panel();
+            this.lblDelete = new System.Windows.Forms.Label();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnForgot = new System.Windows.Forms.Button();
+            this.panelHorizRule1 = new System.Windows.Forms.Panel();
+            this.panelTop.SuspendLayout();
             this.SuspendLayout();
             // 
-            // lblName
+            // panelTop
             // 
-            this.lblName.AutoSize = true;
-            this.lblName.Font = new System.Drawing.Font("Times New Roman", 47.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(291, 250);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(378, 72);
-            this.lblName.TabIndex = 9;
-            this.lblName.Text = "#ACCOUNT";
+            this.panelTop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panelTop.Controls.Add(this.lblPath);
+            this.panelTop.Controls.Add(this.lblDisplay);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(960, 40);
+            this.panelTop.TabIndex = 29;
+            // 
+            // lblPath
+            // 
+            this.lblPath.AutoSize = true;
+            this.lblPath.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPath.Location = new System.Drawing.Point(815, 12);
+            this.lblPath.Name = "lblPath";
+            this.lblPath.Size = new System.Drawing.Size(94, 17);
+            this.lblPath.TabIndex = 8;
+            this.lblPath.Text = "Home/Account";
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(6, 12);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(112, 17);
+            this.lblDisplay.TabIndex = 7;
+            this.lblDisplay.Text = "Account Settings";
+            // 
+            // lblChange
+            // 
+            this.lblChange.AutoSize = true;
+            this.lblChange.Font = new System.Drawing.Font("Century Gothic", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChange.Location = new System.Drawing.Point(62, 187);
+            this.lblChange.Name = "lblChange";
+            this.lblChange.Size = new System.Drawing.Size(311, 39);
+            this.lblChange.TabIndex = 28;
+            this.lblChange.Text = "Change password";
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUsername.Location = new System.Drawing.Point(258, 100);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(128, 30);
+            this.lblUsername.TabIndex = 39;
+            this.lblUsername.Text = "USERNAME";
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl1.Location = new System.Drawing.Point(64, 100);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(186, 30);
+            this.lbl1.TabIndex = 38;
+            this.lbl1.Text = "Your Username is:";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl2.Location = new System.Drawing.Point(64, 253);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(612, 24);
+            this.lbl2.TabIndex = 37;
+            this.lbl2.Text = "Did you forget your password or do you want to change it?";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl3.Location = new System.Drawing.Point(64, 474);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(505, 24);
+            this.lbl3.TabIndex = 36;
+            this.lbl3.Text = "All your data will be lost, there is no turning back.";
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Red;
+            this.btnDelete.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(60, 516);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(320, 50);
+            this.btnDelete.TabIndex = 35;
+            this.btnDelete.Text = "Delete your account";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            // 
+            // panelHorizRule2
+            // 
+            this.panelHorizRule2.BackColor = System.Drawing.Color.Black;
+            this.panelHorizRule2.Location = new System.Drawing.Point(60, 452);
+            this.panelHorizRule2.Name = "panelHorizRule2";
+            this.panelHorizRule2.Size = new System.Drawing.Size(850, 2);
+            this.panelHorizRule2.TabIndex = 32;
+            // 
+            // lblDelete
+            // 
+            this.lblDelete.AutoSize = true;
+            this.lblDelete.Font = new System.Drawing.Font("Century Gothic", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDelete.ForeColor = System.Drawing.Color.Red;
+            this.lblDelete.Location = new System.Drawing.Point(62, 410);
+            this.lblDelete.Name = "lblDelete";
+            this.lblDelete.Size = new System.Drawing.Size(267, 39);
+            this.lblDelete.TabIndex = 31;
+            this.lblDelete.Text = "Delete Account";
+            // 
+            // btnChange
+            // 
+            this.btnChange.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnChange.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnChange.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChange.ForeColor = System.Drawing.Color.Black;
+            this.btnChange.Location = new System.Drawing.Point(386, 295);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(320, 50);
+            this.btnChange.TabIndex = 34;
+            this.btnChange.Text = "Change Password";
+            this.btnChange.UseVisualStyleBackColor = false;
+            // 
+            // btnForgot
+            // 
+            this.btnForgot.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.btnForgot.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnForgot.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnForgot.ForeColor = System.Drawing.Color.Blue;
+            this.btnForgot.Location = new System.Drawing.Point(60, 295);
+            this.btnForgot.Name = "btnForgot";
+            this.btnForgot.Size = new System.Drawing.Size(320, 50);
+            this.btnForgot.TabIndex = 33;
+            this.btnForgot.Text = "Forgot Password";
+            this.btnForgot.UseVisualStyleBackColor = true;
+            // 
+            // panelHorizRule1
+            // 
+            this.panelHorizRule1.BackColor = System.Drawing.Color.Black;
+            this.panelHorizRule1.Location = new System.Drawing.Point(60, 229);
+            this.panelHorizRule1.Name = "panelHorizRule1";
+            this.panelHorizRule1.Size = new System.Drawing.Size(850, 2);
+            this.panelHorizRule1.TabIndex = 30;
             // 
             // UC_studAcc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.panelTop);
+            this.Controls.Add(this.lblChange);
+            this.Controls.Add(this.lblUsername);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.panelHorizRule2);
+            this.Controls.Add(this.lblDelete);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.btnForgot);
+            this.Controls.Add(this.panelHorizRule1);
             this.Name = "UC_studAcc";
             this.Size = new System.Drawing.Size(960, 670);
+            this.panelTop.ResumeLayout(false);
+            this.panelTop.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -56,6 +221,19 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Label lblPath;
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Label lblChange;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Panel panelHorizRule2;
+        private System.Windows.Forms.Label lblDelete;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnForgot;
+        private System.Windows.Forms.Panel panelHorizRule1;
     }
 }
